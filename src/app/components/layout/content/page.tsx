27f9490ed67ac988f4f7bc2850cb0b3b63eb9326
Nicollas Prudencio <67238467+nic37@users.noreply.github.com>
2023-07-08 22:37:37 -0300

    Update page.tsx

--- src/app/components/layout/content/page.tsx
+++ src/app/components/layout/content/page.tsx
@@ -1,4 +1,4 @@
-export default function Content({children = ""}){
+export default function Content({children = <h1>Default</h1>}){
     return (
         <main className="w-full h-full pl-64 pt-5">
             <div className="mx-5 p-3 border-2 border-blue-200 border-dashed rounded-lg dark:border-blue-700">
